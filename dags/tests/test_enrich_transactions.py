612377683cbf5734103f17d5d28432a728abb792
Daniel van der Ende <daniel.vanderende@gmail.com>
2023-09-11 20:00:41 +0200

    Cleaning up

--- dags/tests/test_enrich_transactions.py
+++
@@ -1,9 +0,0 @@
-def test_enrich_transactions(spark, environment):
-    spark.sql("USE {0}_app".format(environment)).collect()
-
-    # Make sure no North Koreans slipped through the DMZ
-    assert spark.sql("""
-    SELECT
-    SUM(IF(payer_country IS NULL OR beneficiary_country IS NULL,1,0)) null_count
-    FROM enrich_transactions
-    """).first().null_count == 0
--- dbt-inferno/whirl.setup.d/01_setup_mockdata_hive.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "=========================="
-echo "== Setup Mockdata       =="
-echo "=========================="
-spark-submit --name spark-data-generate \
-             /opt/airflow/dags/dbt-inferno/spark/generate_data.py
